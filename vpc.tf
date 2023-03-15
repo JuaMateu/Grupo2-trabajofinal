184ed0ef95d2d17dccfac61ffbce0f480ec93a82
FEDERICOFS <federico.o.frias@gmail.com>
2023-03-15 11:45:11 -0300

    "vpc"

---
+++ vpc.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+
+  name = "proyecto-vpc"
+
+  cidr = "10.0.0.0/16"
+
+  azs = ["us-east-1a", "us-east-1b"]
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24"]
+  #permite que las subnet privadas vallan a internet para responder peticiones o actulizar los nodos
+  enable_nat_gateway = true
+  #crea  nat_gateway en todas las subnet privads para alta disponibilida 
+  single_nat_gateway = false
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
