af59045a0ada9b98124062242337bceedd032764
Juan Mateu <juanma.mateu@gmail.com>
2023-03-21 18:18:34 -0300

    se habilita la creacion de EKS

--- Terraform/dev/main.tf
+++ Terraform/dev/main.tf
@@ -21,14 +21,14 @@ module "ec2-instance" {
 
 }
 
-# module "eks" {
-#   source       = "../modules/eks"
-#   eks_name         = var.eks_name
-#   environment = var.environment
-#   vpc_id = module.vpc.vpc_id
-#   private_subnets = module.vpc.private_subnets
-#   private_subnets_control_plane = module.vpc.private_subnets
-# }
+module "eks" {
+  source       = "../modules/eks"
+  eks_name         = var.eks_name
+  environment = var.environment
+  vpc_id = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+  private_subnets_control_plane = module.vpc.private_subnets
+}
 
 module "security-group_ec2" {
     source = "../modules/security-group"
@@ -73,57 +73,3 @@ resource "aws_elb_attachment" "ec2attachment" {
 
 }
 
-
-
-
-
-#  module "eks" {
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "~> 19.0"
-
-#   cluster_name    = "my-cluster"
-#   cluster_version = "1.24"
-
-#   cluster_endpoint_public_access  = true
-
-#   cluster_addons = {
-#     coredns = {
-#       most_recent = true
-#     }
-#     kube-proxy = {
-#       most_recent = true
-#     }
-#     vpc-cni = {
-#       most_recent = true
-#     }
-#   }
-
-#   vpc_id                   = "vpc-06000508380c4e80c"
-#   subnet_ids               = ["subnet-0df0dfcf66046819e","subnet-0f5f611df2df1a5b2"] #siempre privada
-#   control_plane_subnet_ids = ["subnet-0df0dfcf66046819e","subnet-0f5f611df2df1a5b2"] #usar outputs para obtener este valor
-
-
-#   eks_managed_node_groups = {
-#     green = {
-#       min_size     = 1
-#       max_size     = 3
-#       desired_size = 1
-
-#       instance_types = ["t2.micro"]
-#       capacity_type  = "SPOT"
-#     }cim
-#   }
-# }
-#  }
-
-# module "security-group" {
-#   source = "../modules/security-group"
-#   }
-
-# module "iam" {
-#   source = "../modules/iam"
-#   }
-# module "ec2-instance" {
-#   source = "../modules/ec2-instance"
-#   }
