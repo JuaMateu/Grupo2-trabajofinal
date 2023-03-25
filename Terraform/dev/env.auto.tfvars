202f8d8f43470b8bf6352845f07c13153b9fdccf
FEDERICOFS <federico.o.frias@gmail.com>
2023-03-25 13:45:44 -0300

    edit2

--- Terraform/dev/env.auto.tfvars
+++ Terraform/dev/env.auto.tfvars
@@ -1,8 +1,8 @@
 vpc_name    = "dev-vpc-g2"
 environment = "dev"
 
-ec2_name = "dev-jenkins-g2"
+ec2_name     = "dev-jenkins-g2"
 ec2_key_name = "dev-jenkins-key"
-eks_name = "dev-cluster-g2"
+eks_name     = "dev-cluster-g2"
 
 
--- Terraform/dockerinstall.sh
+++ Terraform/dockerinstall.sh
@@ -5,14 +5,14 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
 sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-git clone https://github.com/JuaMateu/Grupo2-trabajofinal.git
-cd Grupo2-trabajofinal/CICD
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo git clone https://github.com/JuaMateu/Grupo2-trabajofinal.git
+sudo mv /Grupo2-trabajofinal/Jenkins/docker-compose.yaml .
 sudo docker compose up -d
-
