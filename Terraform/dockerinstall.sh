dbc8708a3e41d13cffb9457ccb480385ea9b351e
FEDERICOFS <federico.o.frias@gmail.com>
2023-03-26 20:22:15 -0300

    edit4

--- Terraform/dockerinstall.sh
+++ Terraform/dockerinstall.sh
@@ -14,33 +14,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo docker network create jenkins
-sudo docker run \
-  --name jenkins-docker \
-  --rm \
-  --detach \
-  --privileged \
-  --network jenkins \
-  --network-alias docker \
-  --env DOCKER_TLS_CERTDIR=/certs \
-  --volume jenkins-docker-certs:/certs/client \
-  --volume jenkins-data:/var/jenkins_home \
-  --publish 2376:2376 \
-  --publish 3000:3000 --publish 5000:5000 \
-  docker:dind \
-  --storage-driver overlay2 
-sudo docker run \
-  --name jenkins-blueocean \
-  --detach \
-  --network jenkins \
-  --env DOCKER_HOST=tcp://docker:2376 \
-  --env DOCKER_CERT_PATH=/certs/client \
-  --env DOCKER_TLS_VERIFY=1 \
-  --publish 8080:8080 \
-  --publish 50000:50000 \
-  --volume jenkins-data:/var/jenkins_home \
-  --volume jenkins-docker-certs:/certs/client:ro \
-  --volume "$HOME":/home \
-  --restart=on-failure \
-  --env JAVA_OPTS="-Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true" \
-  myjenkins-blueocean:2.387.1-1 
+sudo git clone https://github.com/JuaMateu/Grupo2-trabajofinal.git
+sudo mv /Grupo2-trabajofinal/CICD/docker-compose.yaml .
+sudo docker compose up -d
 
