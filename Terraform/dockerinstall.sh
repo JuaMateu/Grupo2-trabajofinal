2a50f6969dad48a0f6b3c402bd1232cd633cba5b
FEDERICOFS <federico.o.frias@gmail.com>
2023-03-27 14:36:26 -0300

    edit4

--- Terraform/dockerinstall.sh
+++ Terraform/dockerinstall.sh
@@ -26,7 +26,6 @@ sudo docker run --name jenkins-blueocean --detach \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
   --volume jenkins-data:/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
-  --volume "%HOMEDRIVE%%HOMEPATH%":/home \
   --restart=on-failure \
   --env JAVA_OPTS="-Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true" \
   --publish 8080:8080 --publish 50000:50000 misticar/myjenkins-blueocean:2.3
