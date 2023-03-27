235060c3bc520936ff1b25d70a349150a73f9d81
FEDERICOFS <federico.o.frias@gmail.com>
2023-03-27 13:21:37 -0300

    edit4

--- Terraform/dockerinstall.sh
+++ Terraform/dockerinstall.sh
@@ -14,6 +14,20 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo docker network create jenkins
+sudo docker run \
+  --name jenkins-docker \
+  --rm \
+  --detach \
+  --privileged \
+  --network jenkins \
+  --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  --publish 3000:3000 --publish 5000:5000 \
+  docker:dind \
+  --storage-driver overlay2
 sudo git clone https://github.com/JuaMateu/Grupo2-trabajofinal.git
 sudo mv /Grupo2-trabajofinal/CICD/docker-compose.yaml .
 sudo docker compose up -d
